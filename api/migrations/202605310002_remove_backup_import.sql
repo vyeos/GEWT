24acb0c313ab561bbd699f34446938a815865b12
vyeos <rudranpatel0980@gmail.com>
2026-06-01 12:07:42 +0530

    db wiped again

--- api/migrations/202605310002_remove_backup_import.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS backup_audit_logs;
-DROP TABLE IF EXISTS backup_settings;
-
-ALTER TABLE academic_settings
-DROP COLUMN IF EXISTS backups_enabled;
--- api/migrations/202606010001_drop_dropdown_numbering.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS dropdown_masters;
-DROP TABLE IF EXISTS numbering_rules;
